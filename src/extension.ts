ae7ec17ee55aa1d05a6d2d4e8ce16e3b809c76de
Evan Harmon <evan@evanharmon.com>
2022-08-07 18:17:16 -0500

    Add commands to split selected text into new files

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { getCurrentXPath } from "./xml/getCurrentXPath";
+const fs = require('fs');
+const path = require('path');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	// Convert the selected text to dot notation - e.g. convert this/is/a/path to this.is.a.path
-	let dotnotation = vscode.commands.registerCommand('extension.dotnotation', () => {
+	let dotNotation = vscode.commands.registerCommand('extension.dotNotation', () => {
 		let editor = vscode.window.activeTextEditor;
 
 		if (editor) {
@@ -24,14 +26,111 @@ export function activate(context: vscode.ExtensionContext) {
 
 			// Get the text of the selection
 			let textSelection = document.getText(selection);
-			let dotnotation = textSelection.split('/').join('.');
+			let dotNotation = textSelection.split('/').join('.');
 			editor.edit(editBuilder => {
-				editBuilder.replace(selection, dotnotation);
+				editBuilder.replace(selection, dotNotation);
 			});
 		}
 	});
+	context.subscriptions.push(dotNotation);
 
-	context.subscriptions.push(dotnotation);
+	let splitTextIntoFilesUserInput = vscode.commands.registerCommand('extension.splitTextIntoFilesUserInput', async () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let document = editor.document;
+			let selection = editor.selection;
+			// Get the text of the selection
+			let textSelection = document.getText(selection);
+
+			// Get pattern to split the text on from user input box
+			let splitOnPattern = await vscode.window.showInputBox({
+				prompt: "Enter text pattern that you want the text selection to be split on."
+			});
+			
+			// Split the selected text on the splitOnPattern and store in array
+			let arr = textSelection.split(splitOnPattern);
+
+			 // save items as new files
+			arr.forEach((data, idx)=> {
+				let firstRow = data.split(splitOnPattern, 1);
+				const filePath = path.join(vscode.workspace.rootPath, `${firstRow}.md`);
+
+				// write to file
+				fs.writeFileSync(`${filePath}`, data, 'utf8');
+
+				const openPath = vscode.Uri.file(filePath);
+				vscode.workspace.openTextDocument(openPath).then(doc => {
+					vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+				});
+			});
+		}
+	});
+	context.subscriptions.push(splitTextIntoFilesUserInput);
+
+	let splitTextIntoDoubleNewLine = vscode.commands.registerCommand('extension.splitTextIntoDoubleNewLine', async () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let document = editor.document;
+			let selection = editor.selection;
+			// Get the text of the selection
+			let textSelection = document.getText(selection);
+
+			// Pattern to split the text on
+			let splitOnPattern = '\n\n';
+			
+			// Split the selected text on the splitOnPattern and store in array
+			let arr = textSelection.split(splitOnPattern);
+
+			 // save items as new files
+			arr.forEach((data, idx)=> {
+				let firstRow = data.split(splitOnPattern, 1);
+				const filePath = path.join(vscode.workspace.rootPath, `${firstRow}.md`);
+
+				// write to file
+				fs.writeFileSync(`${filePath}`, data, 'utf8');
+
+				const openPath = vscode.Uri.file(filePath);
+				vscode.workspace.openTextDocument(openPath).then(doc => {
+					vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+				});
+			});
+		}
+	});
+	context.subscriptions.push(splitTextIntoDoubleNewLine);
+
+	let splitTextIntoTripleNewLine = vscode.commands.registerCommand('extension.splitTextIntoTripleNewLine', async () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let document = editor.document;
+			let selection = editor.selection;
+			// Get the text of the selection
+			let textSelection = document.getText(selection);
+
+			// Pattern to split the text on
+			let splitOnPattern = '\n\n\n';
+			
+			// Split the selected text on the splitOnPattern and store in array
+			let arr = textSelection.split(splitOnPattern);
+
+			 // save items as new files
+			arr.forEach((data, idx)=> {
+				let firstRow = data.split(splitOnPattern, 1);
+				const filePath = path.join(vscode.workspace.rootPath, `${firstRow}.md`);
+
+				// write to file
+				fs.writeFileSync(`${filePath}`, data, 'utf8');
+
+				const openPath = vscode.Uri.file(filePath);
+				vscode.workspace.openTextDocument(openPath).then(doc => {
+					vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+				});
+			});
+		}
+	});
+	context.subscriptions.push(splitTextIntoTripleNewLine);
 
 	// Function for getting the current XPath of an XML document
     context.subscriptions.push(
